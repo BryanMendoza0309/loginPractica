ce93ef3000bca92191265dd00544feac377dfcec
BryanMendoza0309 <andyloor0309@gmail.com>
2021-01-22 23:26:56 -0500

    Segundo commit

--- coneccionBaseDatos.php
+++ coneccionBaseDatos.php
@@ -2,9 +2,9 @@
     
         $usuario = "admin"; 
         $password = "andyloor123"; 
-        $endpoint = "dblogin.cdy5ocgqwxiq.us-east-1.rds.amazonaws.com";  
+        $endpoint = "dbloginregister.cdy5ocgqwxiq.us-east-1.rds.amazonaws.com";  
         $basedatos = "";
-        $basedatos = "dblogin";
+        $basedatos = "dbloginregister";
         $tabla_db = "usuario";
         $mysqli = new mysqli($endpoint, $usuario, $password, $basedatos);
 
